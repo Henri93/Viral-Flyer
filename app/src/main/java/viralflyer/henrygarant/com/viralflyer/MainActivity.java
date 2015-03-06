aa1eab2701cf54c677e776deb7f2f17993a3e7c3
Henri93 <henryrgarant@gmail.com>
2015-03-06 13:32:14 -0500

    Revert "Create NFC sending"
    This reverts commit 327659132cb07ab01d219f63722d862d92d00c74.

--- app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
+++ app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
@@ -1,72 +1,155 @@
 package viralflyer.henrygarant.com.viralflyer;
 
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.os.Build;
+import android.nfc.Tag;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private NfcAdapter nfcAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mIntentFilters;
+    private String[][] mNFCTechLists;
+    private NdefMessage ndefMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PackageManager pm = this.getPackageManager();
-        // Check whether NFC is available on device
-        if (!pm.hasSystemFeature(PackageManager.FEATURE_NFC)) {
-            // NFC is not available on the device.
-            Toast.makeText(this, "The device does not has NFC hardware.",
-                    Toast.LENGTH_SHORT).show();
+        //Check if the user has NFC
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        //Inform the user of their NFC situation
+        if (nfcAdapter != null && nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC enabled", Toast.LENGTH_SHORT).show();
+        } else if (nfcAdapter != null) {
+            Toast.makeText(this, "Please enable NFC", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "NFC failed", Toast.LENGTH_SHORT).show();
         }
-        // Check whether device is running Android 4.1 or higher
-        else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-            // Android Beam feature is not supported.
-            Toast.makeText(this, "Android Beam is not supported.",
-                    Toast.LENGTH_SHORT).show();
+
+        // create an intent with tag data and deliver to this activity
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        // set an intent filter for all MIME data
+        IntentFilter ndefIntent = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndefIntent.addDataType("*/*");
+            mIntentFilters = new IntentFilter[] { ndefIntent };
+        } catch (Exception e) {
+            Log.e("TagDispatch", e.toString());
         }
-        else {
-            // NFC and Android Beam file transfer is supported.
-            Toast.makeText(this, "Android Beam is supported on your device.",
-                    Toast.LENGTH_SHORT).show();
-        }
+
+        mNFCTechLists = new String[][] { new String[] { NfcF.class.getName() } };
+
+        // create an NDEF message with two records of plain text type
+        ndefMessage = new NdefMessage(
+                new NdefRecord[] {
+                        createTextRecord("First sample NDEF text record", Locale.ENGLISH, true),
+                        createTextRecord("Second sample NDEF text record", Locale.ENGLISH, true) });
     }
 
-    public void sendFile(View view) {
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    @Override
+    protected void onResume() {
+        if (nfcAdapter != null){
+            nfcAdapter.setNdefPushMessage(ndefMessage, this);
+            nfcAdapter.enableForegroundDispatch(this, mPendingIntent, mIntentFilters, mNFCTechLists);
+        }
+        super.onResume();
+    }
 
-        // Check whether NFC is enabled on device
-        if(!nfcAdapter.isEnabled()){
-            // NFC is disabled, show the settings UI
-            // to enable NFC
-            Toast.makeText(this, "Please enable NFC.",
-                    Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+    @Override
+    protected void onPause() {
+        if (nfcAdapter != null){
+            nfcAdapter.disableForegroundDispatch(this);
         }
-        // Check whether Android Beam feature is enabled on device
-        else if(!nfcAdapter.isNdefPushEnabled()) {
-            // Android Beam is disabled, show the settings UI
-            // to enable Android Beam
-            Toast.makeText(this, "Please enable Android Beam.",
-                    Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
-        }
-        else {
-            Log.d("NFC SENDING: ","SENT METHOD");
-            /*NFC and Android Beam both are enabled
-            File to be transferred
+        super.onPause();
+    }
 
-            nfcAdapter.setBeamPushUris(
-                    new Uri[]{Uri.fromFile(fileToTransfer)}, this);*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        String s = action + "\n\n" + tag.toString();
+
+        // parse through all NDEF messages and their records and pick text type only
+        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (data != null) {
+            try {
+                for (int i = 0; i < data.length; i++) {
+                    NdefRecord [] recs = ((NdefMessage)data[i]).getRecords();
+                    for (int j = 0; j < recs.length; j++) {
+                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
+                                Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
+                            byte[] payload = recs[j].getPayload();
+                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+                            int langCodeLen = payload[0] & 0077;
+
+                            s += ("\n\nNdefMessage[" + i + "], NdefRecord[" + j + "]:\n\"" +
+                                    new String(payload, langCodeLen + 1, payload.length - langCodeLen - 1,
+                                            textEncoding) + "\"");
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("TagDispatch", e.toString());
+            }
+        }
+        Log.d("NFC Message", s);
+    }
+
+    public static NdefRecord createTextRecord(String payload, Locale locale, boolean encodeInUtf8) {
+        byte[] langBytes = locale.getLanguage().getBytes(Charset.forName("US-ASCII"));
+        Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset.forName("UTF-16");
+        byte[] textBytes = payload.getBytes(utfEncoding);
+        int utfBit = encodeInUtf8 ? 0 : (1 << 7);
+        char status = (char) (utfBit + langBytes.length);
+        byte[] data = new byte[1 + langBytes.length + textBytes.length];
+        data[0] = (byte) status;
+        System.arraycopy(langBytes, 0, data, 1, langBytes.length);
+        System.arraycopy(textBytes, 0, data, 1 + langBytes.length, textBytes.length);
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                NdefRecord.RTD_TEXT, new byte[0], data);
     }
 }
