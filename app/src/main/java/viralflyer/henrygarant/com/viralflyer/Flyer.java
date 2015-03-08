edacd06241c57fea02cef3390bad44bbec65f725
Henry Garant <henryrgarant@outlook.com>
2015-03-08 19:15:51 -0400

    Add Simulated Event to MainActivity.java

--- app/src/main/java/viralflyer/henrygarant/com/viralflyer/Flyer.java
+++ app/src/main/java/viralflyer/henrygarant/com/viralflyer/Flyer.java
@@ -76,7 +76,7 @@ public class Flyer{
 
     public String toReadableText() {
         return  "Event Name='" + eventName + '\'' +
-                ", Date='" + calendar.getTime().getMonth() + "/" + calendar.getTime().getDate() + calendar.getTime().getYear() +'\'' +
+                ", Date='" + calendar.getTime().getMonth() + "/" + calendar.getTime().getDate() + "/" + calendar.getTime().getYear() +'\'' +
                 ", Time='" + calendar.getTime().getHours() +":"+ calendar.getTime().getMinutes() + '\'' +
                 ", Location='" + location + '\'' +
                 ", Host='" + host + '\'';
