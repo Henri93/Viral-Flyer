859b6eb6399ec26c5512e4f7b771ffeffd705f5e
Henry Garant <henryrgarant@outlook.com>
2015-04-03 12:39:22 -0400

    Rewrite MainActivity

--- app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
+++ app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
@@ -1,34 +1,15 @@
 package viralflyer.henrygarant.com.viralflyer;
 
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.nfc.FormatException;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private NfcAdapter nfcAdapter;
-    PendingIntent pendingIntent;
-    IntentFilter writeTagFilters[];
-    Tag mytag;
-    private Flyer flyer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,82 +35,5 @@ public class MainActivity extends ActionBarActivity {
             Toast.makeText(this, "Android Beam is supported on your device.",
                     Toast.LENGTH_SHORT).show();
         }
-
-        //flyer = new Flyer("Henry", "House Party", new GregorianCalendar(), "3030 Magee Ave");
-        //Log.d("Event: ",flyer.toReadableText());
-        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
-        writeTagFilters = new IntentFilter[] { tagDetected };
-        //write("Henry Was Here!", mytag);
-    }
-
-    public void sendFile(View view) {
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-        // Check whether NFC is enabled on device
-        if(!nfcAdapter.isEnabled()){
-            // NFC is disabled, show the settings UI
-            // to enable NFC
-            Toast.makeText(this, "Please enable NFC.",
-                    Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
-        }
-        // Check whether Android Beam feature is enabled on device
-        else if(!nfcAdapter.isNdefPushEnabled()) {
-            // Android Beam is disabled, show the settings UI
-            // to enable Android Beam
-            Toast.makeText(this, "Please enable Android Beam.",
-                    Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
-        }
-        else {
-            Log.d("NFC SENDING: ","SENT METHOD");
-            /*NFC and Android Beam both are enabled
-            File to be transferred
-
-            nfcAdapter.setBeamPushUris(
-                    new Uri[]{Uri.fromFile(fileToTransfer)}, this);*/
-        }
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent){
-        Log.d("WE HERE","Yes");
-        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
-            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            Toast.makeText(this, "XXX:  " + mytag.toString(), Toast.LENGTH_LONG ).show();
-        }else{
-
-        }
-    }
-
-    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
-
-        //create the message in according with the standard
-        String lang = "en";
-        byte[] textBytes = text.getBytes();
-        byte[] langBytes = lang.getBytes("US-ASCII");
-        int langLength = langBytes.length;
-        int textLength = textBytes.length;
-
-        byte[] payload = new byte[1 + langLength + textLength];
-        payload[0] = (byte) langLength;
-
-        // copy langbytes and textbytes into payload
-        System.arraycopy(langBytes, 0, payload, 1, langLength);
-        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
-
-        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
-        return recordNFC;
-    }
-
-    private void write(String text, Tag tag) throws IOException, FormatException {
-        NdefRecord[] records = { createRecord(text) };
-        NdefMessage message = new NdefMessage(records);
-        Ndef ndef = Ndef.get(tag);
-        ndef.connect();
-        ndef.writeNdefMessage(message);
-        ndef.close();
     }
 }
