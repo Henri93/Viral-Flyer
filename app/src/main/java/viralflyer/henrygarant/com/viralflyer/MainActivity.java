23a5bb9d60c8f5c354ec49ef1d3cbc6079fa1316
Henry Garant <henryrgarant@outlook.com>
2015-04-03 13:01:36 -0400

    Update MainActivity

--- app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
+++ app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
@@ -17,7 +17,9 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -27,6 +29,7 @@ public class MainActivity extends ActionBarActivity {
     private PendingIntent mPendingIntent;
     private IntentFilter[] mIntentFilters;
     private String[][] mNFCTechLists;
+    private NdefMessage mNdefMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +71,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         mNFCTechLists = new String[][] { new String[] { NfcF.class.getName() } };
+
+        // create an NDEF message with two records of plain text type
+        mNdefMessage = new NdefMessage(
+                new NdefRecord[] {
+                        createNewTextRecord("First sample NDEF text record", Locale.ENGLISH, true),
+                        createNewTextRecord("Second sample NDEF text record", Locale.ENGLISH, true) });
     }
 
     @Override
@@ -102,6 +111,23 @@ public class MainActivity extends ActionBarActivity {
         }
 
         mTextView.setText(s);
+    }
+
+    public static NdefRecord createNewTextRecord(String text, Locale locale, boolean encodeInUtf8) {
+        byte[] langBytes = locale.getLanguage().getBytes(Charset.forName("US-ASCII"));
+
+        Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset.forName("UTF-16");
+        byte[] textBytes = text.getBytes(utfEncoding);
+
+        int utfBit = encodeInUtf8 ? 0 : (1 << 7);
+        char status = (char)(utfBit + langBytes.length);
+
+        byte[] data = new byte[1 + langBytes.length + textBytes.length];
+        data[0] = (byte)status;
+        System.arraycopy(langBytes, 0, data, 1, langBytes.length);
+        System.arraycopy(textBytes, 0, data, 1 + langBytes.length, textBytes.length);
+
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], data);
     }
 
 
