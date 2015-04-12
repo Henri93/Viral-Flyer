ef4a33fcbda00cba7dd65e2e5ab718e046e232eb
Henry Garant <henryrgarant@outlook.com>
2015-04-12 12:41:59 -0400

    Adding Navigation

--- app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
+++ app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
@@ -1,163 +1,18 @@
 package viralflyer.henrygarant.com.viralflyer;
 
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.nfc.FormatException;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.nfc.tech.NfcF;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private TextView mTextView;
-    private NfcAdapter mNfcAdapter;
-    private PendingIntent mPendingIntent;
-    private IntentFilter[] mIntentFilters;
-    private String[][] mNFCTechLists;
-    private NdefMessage mNdefMessage;
-    private Tag myTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PackageManager pm = this.getPackageManager();
-        // Check whether NFC is available on device
-        if (!pm.hasSystemFeature(PackageManager.FEATURE_NFC)) {
-            // NFC is not available on the device.
-            Toast.makeText(this, "The device does not has NFC hardware.",
-                    Toast.LENGTH_SHORT).show();
-        }
-        // Check whether device is running Android 4.1 or higher
-        else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-            // Android Beam feature is not supported.
-            Toast.makeText(this, "Android Beam is not supported.",
-                    Toast.LENGTH_SHORT).show();
-        } else {
-            // NFC and Android Beam file transfer is supported.
-            Toast.makeText(this, "Android Beam is supported on your device.",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        mTextView = (TextView) findViewById(R.id.myTextView);
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        // create an intent with tag data and deliver to this activity
-        mPendingIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
-        // set an intent filter for all MIME data
-        IntentFilter ndefIntent = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        try {
-            ndefIntent.addDataType("*/*");
-            mIntentFilters = new IntentFilter[]{ndefIntent};
-        } catch (Exception e) {
-            Log.e("TagDispatch", e.toString());
-        }
-
-        mNFCTechLists = new String[][]{new String[]{NfcF.class.getName()}};
-
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        String action = intent.getAction();
-        myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-
-        String s = "";
-
-        // parse through all NDEF messages and their records and pick text type only
-        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-        if (data != null) {
-            try {
-                for (int i = 0; i < data.length; i++) {
-                    NdefRecord[] recs = ((NdefMessage) data[i]).getRecords();
-                    for (int j = 0; j < recs.length; j++) {
-                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
-                                Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
-                            byte[] payload = recs[j].getPayload();
-                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
-                            int langCodeLen = payload[0] & 0077;
-
-                            s += " " + new String(payload, langCodeLen + 1, payload.length - langCodeLen - 1,
-                                    textEncoding);
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                Log.e("TagDispatch", e.toString());
-            }
-        }
-
-        mTextView.setText(s);
-    }
-
-    public static NdefRecord createNewTextRecord(String text, Locale locale, boolean encodeInUtf8) {
-        byte[] langBytes = locale.getLanguage().getBytes(Charset.forName("US-ASCII"));
-
-        Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset.forName("UTF-16");
-        byte[] textBytes = text.getBytes(utfEncoding);
-
-        int utfBit = encodeInUtf8 ? 0 : (1 << 7);
-        char status = (char) (utfBit + langBytes.length);
-
-        byte[] data = new byte[1 + langBytes.length + textBytes.length];
-        data[0] = (byte) status;
-        System.arraycopy(langBytes, 0, data, 1, langBytes.length);
-        System.arraycopy(textBytes, 0, data, 1 + langBytes.length, textBytes.length);
-
-        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], data);
-    }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mNfcAdapter != null)
-            mNfcAdapter.enableForegroundDispatch(this, mPendingIntent, mIntentFilters, mNFCTechLists);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (mNfcAdapter != null)
-            mNfcAdapter.disableForegroundDispatch(this);
-    }
-
-    public void PushMessage(View v) {
-        // create an NDEF message with two records of plain text type
-        mNdefMessage = new NdefMessage(
-                new NdefRecord[]{
-                        createNewTextRecord("Henry Garant \n (267)-670-0999 \n henryrgarant@gmail.com \n Android Developer", Locale.ENGLISH, true)});
-
-        Ndef ndef = Ndef.get(myTag);
-        try {
-            ndef.connect();
-            ndef.writeNdefMessage(mNdefMessage);
-            ndef.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (FormatException e) {
-            e.printStackTrace();
-        }
     }
 }
+
+
