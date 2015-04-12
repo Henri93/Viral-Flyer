ba61bca060dbb77291c9af29bba71eccbc348fc4
Henry Garant <henryrgarant@outlook.com>
2015-04-12 12:46:38 -0400

    Adding Navigation

--- app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
+++ app/src/main/java/viralflyer/henrygarant/com/viralflyer/MainActivity.java
@@ -1,7 +1,9 @@
 package viralflyer.henrygarant.com.viralflyer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +13,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void read(View v){
+        //go to read activity
+        Intent intent = new Intent(this, Reader.class);
+        startActivity(intent);
+    }
+
+    public void write(View v){
+        //go to write activity
+        Intent intent = new Intent(this, Writer.class);
+        startActivity(intent);
     }
 }
 
